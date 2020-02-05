4cee696168760a86bdf13565b2daa4bfb5beb2cf
Mubariz Khan <37043727+MubarizKhan@users.noreply.github.com>
2020-02-05 17:08:03 +0500

    Add files via upload

--- Assignment 1/multiply_dc.py
+++ Assignment 1/multiply_dc.py
@@ -22,8 +22,6 @@ def regroup(list2):
 
 
 
-    
-
 def div_conq(list1, list2):
     list_evenify(list1)
     list_evenify(list2)
@@ -38,55 +36,32 @@ def div_conq(list1, list2):
 
     for i in range(0,len(l1)):
 
-        print(l1[i], "L1")
         shift = (len(l1)- i ) 
-        # print(shift, "<---shift")
-        # shift = 10 ** shift
-        # print(shift, "<---shift")
         mulitple_1 = int(l1[i])
-        # print(mulitple_1, "m1")
 
         for j in range(0, len(l2)):
-            # print(l2[j])
             mulitple_2 = int(l2[j])
-            # print(mulitple_2, "m2")
             summation = mulitple_1 * mulitple_2
 
             shift2 = (len(l2) - j ) 
-            print(shift2, "<---shift2222")
             shift2 *= shift
 
-            print(shift, "<--shift1")
+            # print(shift, "<--shift1")
             if shift2 != 1:
-
                 shift2 = 10 ** shift2
-                print(shift2, "<--/-shift2222")
-            
-
                 summation *= shift2
-                print(summation, "<---summation")
                 ter1 += summation
             else:
-                # shift2 = 10 ** shift2
-                print(shift2, "<--/-shift2222 In else")
-            
-
                 summation *= shift2
-                print(summation, "<---summation")
                 ter1 += summation
 
 
-    # return ter1
-    print(ter1, "ter1")
-    
-    # print("end")
-    
-    # print(l1)
-    # print(l2)
+    print(ter1, "This is the product")
 
 
 
-list1 = [9,8,1]
+
+list1 = [-9,8,1]
 list2 = [1,2,3,4]
 
 # print( len(list1))
